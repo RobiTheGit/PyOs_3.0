c78ef9dc370323293f4395c99df34ac572c6ce67
RobiTheGit <94720060+RobiTheGit@users.noreply.github.com>
2023-02-20 11:31:43 -0700

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,11 @@
+cnames = ['white 0', 'cyan 1', 'magenta 2', 'blue 3', 'yellow 4', 'green 5', 'red 6']
+ccodes = ['[1;37;40m', '[1;36;40m', '[1;35;40m', '[1;34;40m', '[1;33;40m', '[1;32;40m', '[1;31;40m']
+disptest ="""
+\033[1;31;40m Bright Red     \033[0m [1;31;40m
+\033[1;32;40m Bright Green   \033[0m [1;32;40m
+\033[1;33;40m Yellow         \033[0m [1;33;40m
+\033[1;34;40m Bright Blue    \033[0m [1;34;40m
+\033[1;35;40m Bright Magenta \033[0m [1;35;40m
+\033[1;36;40m Bright Cyan    \033[0m [1;36;40m
+\033[1;37;40m White          \033[0m [1;37;40m
+"""
